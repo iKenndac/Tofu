18582a309fed28a33075d1bf6fa9f441778c2512
ThinkChaos <ThinkChaos@users.noreply.github.com>
2019-05-01 21:03:29 +0200

    Add otpauth URL Scheme handling
    Fixes #8

--- Tofu/AppDelegate.swift
+++ Tofu/AppDelegate.swift
@@ -10,4 +10,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+
+    func application(_ application: UIApplication,
+                     open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any] = [:] ) -> Bool {
+
+        let rootViewController = window!.rootViewController!
+
+        guard let account = Account(url: URL(fileURLWithPath: ".")) else {
+            let alert = UIAlertController(
+                title: "Could not import account",
+                message: "The account information was not of the expected format.",
+                preferredStyle: UIAlertControllerStyle.alert
+            )
+
+            alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+
+            rootViewController.present(alert, animated: true, completion: nil)
+            return false
+        }
+
+        let accountsViewController = rootViewController.childViewControllers
+            .filter({ $0 is AccountsViewController })
+            .map({ $0 as! AccountsViewController })
+            .first!
+        
+        accountsViewController.createAccount(account)
+        self.noticeOnlyText("Added account '\(account.description)'")
+
+        return true
+    }
 }
