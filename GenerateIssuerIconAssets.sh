530d4ea5ffa5846ddf8589afea6e5f40fedac8ea
FIGBERT <figbert@figbert.com>
2021-01-20 19:00:01 -0800

    Add functionality to GenerateIssuerIconAssets.sh

--- GenerateIssuerIconAssets.sh
+++ GenerateIssuerIconAssets.sh
@@ -12,5 +12,32 @@ for file in ./IssuerIcons/*.png; do
   #   Positive Lookforward -- (?=\.png)
   #     Asserts that the selected portion must be followed by .png
   name="$(echo $file | ag --only-matching '(?<=\.\/IssuerIcons\/).*(?=\.png)')"
+  (mkdir "./Tofu/Assets.xcassets/${name}.imageset" || true) 2>/dev/null
+  cp "$file" "./Tofu/Assets.xcassets/${name}.imageset/${name}@3x.png"
+  sips --resampleWidth 128 "./Tofu/Assets.xcassets/${name}.imageset/${name}@3x.png" --out "./Tofu/Assets.xcassets/${name}.imageset/${name}@2x.png" &>/dev/null
+  sips --resampleWidth 64 "./Tofu/Assets.xcassets/${name}.imageset/${name}@3x.png" --out "./Tofu/Assets.xcassets/${name}.imageset/${name}.png" &>/dev/null
+  echo -n "{
+  \"images\" : [
+    {
+      \"idiom\" : \"universal\",
+      \"filename\" : \"${name}.png\",
+      \"scale\" : \"1x\"
+    },
+    {
+      \"idiom\" : \"universal\",
+      \"filename\" : \"${name}@2x.png\",
+      \"scale\" : \"2x\"
+    },
+    {
+      \"idiom\" : \"universal\",
+      \"filename\" : \"${name}@3x.png\",
+      \"scale\" : \"3x\"
+    }
+  ],
+  \"info\" : {
+    \"version\" : 1,
+    \"author\" : \"xcode\"
+  }
+}" > "./Tofu/Assets.xcassets/${name}.imageset/Contents.json"
 done
 
