d0901415832be8e489f18444a39d699ff9fbac77
Pavel Kirkovsky <pavel@kirkovsky.com>
2023-08-26 00:20:35 -0700

    Add icon for Carta (carta.com)

--- Tofu/AccountCell.swift
+++ Tofu/AccountCell.swift
@@ -29,6 +29,7 @@ private let imageNames: [CaseInsensitiveString: String] = [
     "Bitstamp": "Bitstamp",
     "Bittrex": "Bittrex",
     "Bitwarden": "Bitwarden",
+    "Carta","Carta",
     "Cloudflare": "Cloudflare",
     "Coinbase": "Coinbase",
     "Contentful": "Contentful",
